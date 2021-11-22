feed2f253e2ae002f047b559744b11b71af5d014
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2021-11-22 15:11:11 -0600

    update

---
+++ working-with-types.js
@@ -0,0 +1,20 @@
+var a = 1;
+var b = a++;
+var c = ++a;
+// what is the value of a, b, and c?
+console.log(a);
+console.log(b);
+console.log(c);
+
+var d = "hello";
+var e = false;
+d++;
+e++;
+console.log(d);
+console.log(e);
+
+var perplexed; // perplexed is undefined (no value is assigned)
+console.log(perplexed + 2);
+
+var price = 2.7;
+console.log(price.toFixed(2));
