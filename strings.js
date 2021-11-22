938c59a16e98083c576b181bd48f731574fcfee0
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2021-11-22 12:47:40 -0600

    exercises

---
+++ strings.js
@@ -0,0 +1,6 @@
+var myName = "Kat";
+var game1 = "Final Fantasy XIV";
+var game2 = "Mass Effect";
+var aSentence = "Hi, my name is " + myName + "\nand I like to play games like " + game1 + "\nand " + game2;
+
+console.log(aSentence)
