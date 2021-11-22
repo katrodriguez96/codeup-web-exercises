a8ed7772a642f4a02fc89643f8cd79d6d2b4cefd
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2021-11-22 15:34:24 -0600

    update

--- working-with-types.js
+++ working-with-types.js
@@ -1,4 +1,4 @@
-var a = 1;
+/*var a = 1;
 var b = a++;
 var c = ++a;
 // what is the value of a, b, and c?
@@ -18,3 +18,22 @@ console.log(perplexed + 2);
 
 var price = 2.7;
 console.log(price.toFixed(2));
+
+// var price = "2.7";
+// console.log(price.toFixed(2))*/;
+
+console.log(isNaN(0));
+console.log(isNaN(1));
+console.log(isNaN(""));
+console.log(isNaN("string"));
+console.log(isNaN("0"));
+console.log(isNaN("1"));
+console.log(isNaN("3.145"));
+console.log(isNaN(Number.MAX_VALUE));
+console.log(isNaN(Infinity));
+console.log(isNaN("true"));
+console.log(isNaN(true));
+console.log(isNaN("false"));
+console.log(isNaN(false));
+// to illustrate why the isNaN() function is needed:
+console.log(NaN == NaN);
