ef9ed1b2d606f87b010a17f501ea5d034aae7be2
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2021-11-23 12:19:41 -0600

    update

--- functions warmup.js
+++ functions warmup.js
@@ -1,3 +1,4 @@
+/*
 // Erstwhile, the legislature determined that being at the bar is not necessary in order to be served, but all the
 // remaining conditions are still valid
 // Change the expression assigned to cashOnHand to make isAtBar optional  while still requiring the remaining values
@@ -17,4 +18,32 @@ if (canHaveADrink && isAtBar) { // true or false
     console.log("To go beer for u");
 } else {
     console.log("No brew for u");
+}
+
+function flyDay (isCloudy, numberofPlanes) {
+    if (!isCloudy && numberofPlanes < 10) {
+        console.log("Today's a good day to fly");
+    } else {
+        console.log("Stay grounded");
+    }
+}*/
+
+function addition (x , y) {
+    console.log(x + y);
+}
+
+function subtraction (x , y) {
+    console.log(x - y);
+}
+
+function multiply (x , y) {
+    console.log(x * y);
+}
+
+function divide (x , y) {
+    console.log(x / y);
+}
+
+function remainder (x , y) {
+    console.log(x % y);
 }
