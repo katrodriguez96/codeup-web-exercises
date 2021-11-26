706033de2edbc05f5368038fde1b11a398a00f88
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2021-11-26 14:16:36 -0600

    update

--- js/function-extra-drills.js
+++ js/function-extra-drills.js
@@ -49,6 +49,48 @@ function isMultipleOf (target, n) {
 //console.log(isMultipleOf(9, 3));
 
 function isTrue (boolean) {
-    return boolean == true;
+    return boolean === true;
 }
-console.log(isTrue(50));
+// console.log(isTrue(1));
+
+function isFalse (boolean) {
+    return boolean === false;
+}
+//console.log(isFalse(true));
+
+function isTruthy (boolean) {
+    return !!boolean;
+}
+//console.log(isTruthy(1));
+
+function isFalsy (boolean) {
+    return !boolean;
+}
+//console.log(isFalsy(false));
+
+function isVowel (letter) {
+    letter = letter.toLowerCase()
+    return letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u";
+}
+//console.log(isVowel("O"));
+
+function isConsonant (letter) {
+    return !isVowel(letter);
+}
+//console.log(isConsonant("z"));
+
+function isCapital (letter) {
+    return letter === letter.toUpperCase();
+}
+//console.log(isCapital("C"));
+
+function isLowerCase (letter) {
+    return letter === letter.toLowerCase();
+}
+//console.log(isLowerCase("B"));
+
+function hasLowerCase (string) {
+    return !string.search("".toLowerCase());
+}
+// having trouble with this one
+console.log(hasLowerCase("HAZEL"));
