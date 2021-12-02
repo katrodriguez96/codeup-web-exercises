e916d8e5a7138de99e46cae077ea02291696ba36
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2021-12-02 16:38:28 -0600

    update

---
+++ js/for_loops.js
@@ -0,0 +1,23 @@
+// function showMultiplicationTable (input) {
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(input + " x " + i + " = " + (input * i));
+//     }
+// }
+// showMultiplicationTable(7);
+
+// for (let i = 1; i <= 10; i++) {
+//     let x = Math.floor(Math.random() * (200 - 20) + 20);
+//     if (x % 2 === 0) {
+//         console.log(x + " is an even number.");
+//     } else {
+//         console.log(x + " is an odd number.");
+//     }
+// }
+
+// for (let i = 1; i < 10; i++) {
+//     console.log(i.toString().repeat(i));
+// }
+
+for (let i = 100; i >= 5; i-= 5) {
+    console.log(i);
+}
