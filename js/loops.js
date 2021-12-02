879936ab0edd1db398dd41c72b7bcff0996692a6
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2021-12-02 13:35:05 -0600

    update

---
+++ js/loops.js
@@ -0,0 +1,34 @@
+// 0 available spots
+// 20 total spots
+// loop until parking spot available
+// iterating through the loop
+// there are x number of spots available
+
+// let timePassed = 0;
+// let availableSpots = 0;
+//
+// while (availableSpots === 0) {
+//     if (availableSpots === 0) {
+//         console.log("No spots available after " + timePassed + " minutes of searching.");
+//     } else {
+//         console.log("Finally found an empty spot after " + timePassed + " minutes of searching.")
+//     }
+//     timePassed++;
+//     if (timePassed === 5) {
+//         let availableSpots = 1;
+//     }
+// } infinite loop orz
+
+// loop through numbers 0 - 50 by 2s
+
+let i = 0
+
+while (i < 10) {
+    console.log("i = ", i);
+    i++;
+}
+
+do {
+    console.log("i = ", i);
+    i++;
+} while (i < 10);
