d8159e923b663a311dea67fe5ec2ac6918161e4f
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2021-12-02 18:29:35 -0600

    update

---
+++ js/break_and_continue.js
@@ -0,0 +1,8 @@
+let badNumber = prompt("Please enter an odd number anywhere from 1 to 50.");
+for (let i = 1; i <=50; i+=2) {
+    if (badNumber % 2 !== 0) {
+        console.log("Number to skip: " + badNumber);
+    } else {
+        let badNumber = prompt("Incorrect. Please try t")
+    }
+}
