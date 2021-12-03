8126a9543667cb6f81e241a8e5975ef4f4108c7e
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2021-12-03 09:45:30 -0600

    break and continue exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,8 +1,23 @@
-let badNumber = prompt("Please enter an odd number anywhere from 1 to 50.");
-for (let i = 1; i <=50; i+=2) {
-    if (badNumber % 2 !== 0) {
-        console.log("Number to skip: " + badNumber);
+let badNumber = 0;
+do {
+    badNumber = Number(prompt("Please enter an odd number anywhere from 1 to 50."));
+    if (badNumber < 1 || badNumber > 50) {
+        alert("Number out of bounds. Please try again.");
+    } else if (badNumber % 2 === 0) {
+        alert("That is not an odd number. Please try again.");
+    } else if (isNaN(badNumber)) {
+        alert("That is not a number. Please try again.");
     } else {
-        let badNumber = prompt("Incorrect. Please try t")
+        break;
     }
+} while (true);
+
+console.log("Number to skip is: " + badNumber);
+
+for (let i = 1; i <= 50; i+=2) {
+    if (i === badNumber) {
+        console.log("Yikes! Skipping number: " + badNumber);
+        continue;
+    }
+    console.log("Here is an odd number: " + i);
 }
