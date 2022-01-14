4943794e9a43839acbdc840815c6f8a27ba1cb38
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-01-14 09:20:45 -0600

    warmup not finished

---
+++ js/for_loops_warmup.js
@@ -0,0 +1,9 @@
+// Make a program that filters through this array: ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"] and returns a new array that
+// contains only those whose name have only 4 letters in them.
+
+let oldArray = ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"];
+
+for (let i = 0; i > oldArray.length; i++) {
+    let nameArray = oldArray[i].split("");
+    if (nameArray.length = 3)
+}
