ea21e09d119e8df2a9ef6f4cf63e4a934d3d6b4b
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-01-14 13:03:24 -0600

    for loops warmup complete

--- js/for_loops_warmup.js
+++ js/for_loops_warmup.js
@@ -2,8 +2,17 @@
 // contains only those whose name have only 4 letters in them.
 
 let oldArray = ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"];
+console.log(oldArray)
 
-for (let i = 0; i > oldArray.length; i++) {
+for (let i = oldArray.length - 1; i >= 0; i--) {
     let nameArray = oldArray[i].split("");
-    if (nameArray.length = 3)
+    console.log(nameArray);
+    console.log("How many letters: " + nameArray.length);
+    if (nameArray.length !== 4) {
+        oldArray.splice(i, 1);
+        console.log(oldArray);
+    }
+    else {
+        console.log(oldArray);
+    }
 }
