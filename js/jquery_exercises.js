d5a5b9fbaab4655800582ff434323469ca7d0983
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-01-20 10:53:17 -0600

    jquery selectors exercise complete

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,16 @@
 "use strict";
 
-$(document).ready(function () {
-   alert("The DOM has finished loading.");
-});
+// $(document).ready(function () {
+//    alert("The DOM has finished loading.");
+// });
+
+// let contents = $("#header").html();
+// alert(contents);
+
+// $(".codeup").css("border", "1px solid red");
+
+$("li").css("font-size", "20px");
+$("h1, p, li").css("background-color", "lightyellow");
+
+let contents = $("h1").html();
+alert(contents);
