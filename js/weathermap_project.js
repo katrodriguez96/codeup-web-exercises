a40150ef79fd0d72fb8e04a49fee97405ca7b701
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-01-31 17:06:42 -0600

    weathermap start 5day

--- js/weathermap_project.js
+++ js/weathermap_project.js
@@ -1,23 +1,37 @@
-// single day forecast
-$.get("http://api.openweathermap.org/data/2.5/weather", {
+// // single day forecast
+// $.get("http://api.openweathermap.org/data/2.5/weather", {
+//     APPID: WEATHERMAP_TOKEN,
+//     q:     "San Antonio, US",
+//     units: "imperial"
+// }).done(function(data) {
+//     console.log(data);
+//     let city = data.name;
+//     let tempHigh = data.main.temp_max;
+//     let tempLow = data.main.temp_min;
+//     let weather = data.weather[0].description;
+//     let humidity = data.main.humidity;
+//     let wind = data.wind.speed;
+//     let pressure = data.main.pressure;
+//     $(".container").append(`
+//         <h3>${city} Weather</h3>
+//         <p>High: ${tempHigh}°F / Low: ${tempLow}°F</p>
+//         <p>Description: ${weather}</p>
+//         <p>Humidity: ${humidity}%</p>
+//         <p>Wind speed: ${wind} mph</p>
+//         <p>Pressure: ${pressure} hPa</p>
+//     `)
+// });
+
+// 5-day forecast
+$.get("http://api.openweathermap.org/data/2.5/forecast", {
     APPID: WEATHERMAP_TOKEN,
-    q:     "San Antonio, US",
+    q: "San Antonio, US",
     units: "imperial"
-}).done(function(data) {
-    console.log(data);
-    let city = data.name;
-    let tempHigh = data.main.temp_max;
-    let tempLow = data.main.temp_min;
-    let weather = data.weather[0].description;
-    let humidity = data.main.humidity;
-    let wind = data.wind.speed;
-    let pressure = data.main.pressure;
-    $(".container").append(`
-        <h3>${city} Weather</h3>
-        <p>High: ${tempHigh}°F / Low: ${tempLow}°F</p>
-        <p>Description: ${weather}</p>
-        <p>Humidity: ${humidity}%</p>
-        <p>Wind speed: ${wind} mph</p>
-        <p>Pressure: ${pressure} hPa</p>
-    `)
-});
+}).done(function (data) {
+    console.log(data)
+    let city = data.city.name;
+    for (i = 0; i < data.list.length; i++) {
+        let dt = data.list[i].dt_text;
+        // log data for all 40 then try to increment for each day
+    }
+})
