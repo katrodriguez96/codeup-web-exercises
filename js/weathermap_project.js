f1b21f89aa2497ef88ed72d000b458c51727259a
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-02-01 10:23:25 -0600

    navbar added

--- js/weathermap_project.js
+++ js/weathermap_project.js
@@ -1,27 +1,3 @@
-// // single day forecast
-// $.get("http://api.openweathermap.org/data/2.5/weather", {
-//     APPID: WEATHERMAP_TOKEN,
-//     q:     "San Antonio, US",
-//     units: "imperial"
-// }).done(function(data) {
-//     console.log(data);
-//     let city = data.name;
-//     let tempHigh = data.main.temp_max;
-//     let tempLow = data.main.temp_min;
-//     let weather = data.weather[0].description;
-//     let humidity = data.main.humidity;
-//     let wind = data.wind.speed;
-//     let pressure = data.main.pressure;
-//     $(".container").append(`
-//         <h3>${city} Weather</h3>
-//         <p>High: ${tempHigh}°F / Low: ${tempLow}°F</p>
-//         <p>Description: ${weather}</p>
-//         <p>Humidity: ${humidity}%</p>
-//         <p>Wind speed: ${wind} mph</p>
-//         <p>Pressure: ${pressure} hPa</p>
-//     `)
-// });
-
 // 5-day forecast
 $.get("http://api.openweathermap.org/data/2.5/forecast", {
     APPID: WEATHERMAP_TOKEN,
@@ -30,6 +6,7 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
 }).done(function (data) {
     console.log(data)
     let city = data.city.name;
+    $(".navbar-text").append(`Current city: ${city}`)
     for (i = 0; i < data.list.length; i+= 8) {
         console.log(data.list[i]);
         let date = data.list[i].dt_txt;
@@ -57,3 +34,27 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
         `)
     }
 })
+
+// // single day forecast
+// $.get("http://api.openweathermap.org/data/2.5/weather", {
+//     APPID: WEATHERMAP_TOKEN,
+//     q:     "San Antonio, US",
+//     units: "imperial"
+// }).done(function(data) {
+//     console.log(data);
+//     let city = data.name;
+//     let tempHigh = data.main.temp_max;
+//     let tempLow = data.main.temp_min;
+//     let weather = data.weather[0].description;
+//     let humidity = data.main.humidity;
+//     let wind = data.wind.speed;
+//     let pressure = data.main.pressure;
+//     $(".container").append(`
+//         <h3>${city} Weather</h3>
+//         <p>High: ${tempHigh}°F / Low: ${tempLow}°F</p>
+//         <p>Description: ${weather}</p>
+//         <p>Humidity: ${humidity}%</p>
+//         <p>Wind speed: ${wind} mph</p>
+//         <p>Pressure: ${pressure} hPa</p>
+//     `)
+// });
