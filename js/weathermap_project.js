3a1d469f2c6fd78ae955291376638de390e35f93
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-02-01 10:06:03 -0600

    5 day forecast done

--- js/weathermap_project.js
+++ js/weathermap_project.js
@@ -30,8 +30,30 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
 }).done(function (data) {
     console.log(data)
     let city = data.city.name;
-    for (i = 0; i < data.list.length; i++) {
-        let dt = data.list[i].dt_text;
-        // log data for all 40 then try to increment for each day
+    for (i = 0; i < data.list.length; i+= 8) {
+        console.log(data.list[i]);
+        let date = data.list[i].dt_txt;
+        let tempHigh = data.list[i].main.temp_max;
+        let tempLow = data.list[i].main.temp_min;
+        let weather = data.list[i].weather[0].description;
+        let humidity = data.list[i].main.humidity;
+        let wind = data.list[i].wind.speed;
+        let pressure = data.list[i].main.pressure;
+        let icon = data.list[i].weather[0].icon;
+        $(".card-group").append(`
+            <div class="card">
+                <div class="card-header text-center">${date}</div>
+                <div class="card-body text-center">
+                    <h6>High: ${tempHigh}°F / Low: ${tempLow}°F</h6>
+                    <img src="http://openweathermap.org/img/w/${icon}.png" alt="weather icon">
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Description: ${weather}</li>
+                    <li class="list-group-item">Humidity: ${humidity}%</li>
+                    <li class="list-group-item">Wind: ${wind} mph</li>
+                    <li class="list-group-item">Pressure: ${pressure} hPa</li>
+                </ul>
+            </div>
+        `)
     }
 })
