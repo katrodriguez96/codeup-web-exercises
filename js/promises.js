61c827691651289c18af352a23985843de52af63
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-02-09 09:49:59 -0600

    first half of promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,7 +1,15 @@
+"use strict";
+
 function lastCommit (username) {
-    fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token ${GITHUB_TOKEN}`}})
-        .then(response => console.log(response))
+    fetch(`https://api.github.com/users/${username}/events`)
+        .then(response => {
+        response.json().then(events => {
+            let dateAndTime = events[0].created_at;
+            console.log("Your last push was at: " + (new Date(dateAndTime)).toLocaleString())
+        }).catch(() => console.log("You hit the max API usage"))
+    })
         .catch(error => console.log(error));
 }
 
 lastCommit("katrodriguez96");
+
