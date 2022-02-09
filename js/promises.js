63f8509a56c6e8806a9d549330dd3d473a54b71d
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-02-09 10:43:20 -0600

    promises exercise complete

--- js/promises.js
+++ js/promises.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function lastCommit (username) {
-    fetch(`https://api.github.com/users/${username}/events`)
+    fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token ${GITHUB_TOKEN}`}})
         .then(response => {
         response.json().then(events => {
             let dateAndTime = events[0].created_at;
@@ -13,3 +13,18 @@ function lastCommit (username) {
 
 lastCommit("katrodriguez96");
 
+function wait (number) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.1) {
+                resolve("Here is your data: ...")
+            } else {
+                reject("Network Connection Error!")
+            }
+        }, number)
+    })
+}
+
+wait(1000).then(() => console.log("You'll see this after 1 second."))
+wait(2000).then(() => console.log("You'll see this after 2 seconds."))
+wait(5500).then(() => console.log("You'll see this after 5 seconds and 500 milliseconds."))
